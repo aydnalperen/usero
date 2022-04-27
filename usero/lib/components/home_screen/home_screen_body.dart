287e824b201fcc5a4949dc9d20742763b680c0a5
aydina19 <aydina19@itu.edu.tr>
2022-04-27 17:42:25 +0300

    add app bar message

---
+++ usero/lib/components/home_screen/home_screen_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreenBody extends StatefulWidget {
+  HomeScreenBody({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreenBody> createState() => _HomeScreenBodyState();
+}
+
+class _HomeScreenBodyState extends State<HomeScreenBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
